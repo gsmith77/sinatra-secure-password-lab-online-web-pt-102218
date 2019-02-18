f86bfd63a3f9751583519619477ea5b31b308c86
grant smith <grantsmith1256@gmail.com>
2019-02-18 20:05:33 +0000

    Automatically backed up by Learn

--- db/migrate/20190218200402_create_users.rb
+++ db/migrate/20190218200402_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.username :string
-      t.password :string
+      t.string :username
+      t.string :password
       t.float :balance
     end
   end
