8af2dc6d28fac4110968cbfa68d3eac59c6f76d2
grant smith <grantsmith1256@gmail.com>
2019-02-18 20:04:47 +0000

    Automatically backed up by Learn

--- db/migrate/20190218200402_create_users.rb
+++ db/migrate/20190218200402_create_users.rb
@@ -1,4 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.username :string
+      t.password :string
+      t.float :balance
+    end
   end
 end
